3e9a3017a70936c9e897ceec3a6c5a7845e9bf78
Naren Mudivarthy <narenuday595@gmail.com>
2016-10-07 11:49:31 -0700

    Directly pulling oh-my-zsh to root folder

--- get_plugins.sh
+++ get_plugins.sh
@@ -1,9 +1,9 @@
 
 echo "Cloning oh-my-zsh"
-git clone https://github.com/robbyrussell/oh-my-zsh.git ~/dotfiles/.oh-my-zsh
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 echo "Cloning zsh syntax highlighting "
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/dotfiles/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 echo "Cloning z for quick navigation through folders"
 git clone https://github.com/rupa/z.git ~/dotfiles/z
