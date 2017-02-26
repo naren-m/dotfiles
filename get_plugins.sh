df33915750b71e28c26aec7b380414bc28a9b8f5
Naren Mudivarthy <nmudivar@cisco.com>
2017-02-26 14:05:33 -0800

    zsh syntax highligting setup

--- get_plugins.sh
+++ get_plugins.sh
@@ -3,7 +3,9 @@ echo "Cloning oh-my-zsh"
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 echo "Cloning zsh-syntax-highlighting "
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 
 echo "Cloning zsh-autosuggestions (fish-like) "
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
