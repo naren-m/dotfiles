ccb58cca957da8089ac75aa55d49d1e1367a31cd
Naren Mudivarthy <nmudivar@cisco.com>
2017-10-24 01:31:06 -0700

    cleaned get_plugins, integrated with gtm.

--- get_plugins.sh
+++ get_plugins.sh
@@ -1,9 +1,7 @@
-
 echo "Cloning oh-my-zsh"
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
-echo "Cloning zsh-syntax-highlighting "
-# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+echo "Cloning zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 echo "Cloning z for quick navigation through folders"
@@ -15,7 +13,3 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.
 
 echo "Cloning fast-syntax-highlighting"
 git clone https://github.com/zdharma/fast-syntax-highlighting.git  ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
-
-# Vim plugins
-# echo "Cloning nerdTree for vim"
-# git clone https://github.com/scrooloose/nerdtree ~/dotfiles/.vim/bundle/nerdtree
